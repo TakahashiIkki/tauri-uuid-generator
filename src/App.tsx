e9911c11fb3638daf5ed376e1058c9d341109d84
Takashi Ikki <new_ittyandesu@outlook.jp>
2022-06-19 12:16:42 +0900

    uuid packge install コード全体書いた

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import { v4 as generateUuidV4 } from 'uuid';
+import { HiClipboardCopy } from 'react-icons/hi';
 
-function App() {
+export const App = () => {
+  const [uuid, setUuid] = React.useState<string>('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+      UUID v4 のUUIDを生成します
+      <button
+        onClick={() => {
+          setUuid(generateUuidV4());
+        }}
+      >
+        生成
+      </button>
+      <div>
+        <input type="text" readOnly value={uuid} size={36} />
+        <button
+          onClick={() => {
+            navigator.clipboard.writeText(uuid);
+          }}
+        >
+          <HiClipboardCopy />
+        </button>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
